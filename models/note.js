4bda6154f01053a20db2d5bc14a9086e5f349dd0
Daniele Quasimodo <ICTS21-23.241@studenti.local>
2022-06-16 17:41:48 +0200

    Aggiunto user.js in models

--- models/note.js
+++ models/note.js
@@ -5,32 +5,32 @@ const NoteSchema = new mongoose.Schema(
     user: {
       type: String,
       required: true,
-      trim: true,
+      trim: true
     },
     title: {
       type: String,
       required: true,
-      trim: true,
+      trim: true
     },
     content: {
       type: String,
       required: true,
-      trim: true,
+      trim: true
     },
     public: {
       type: Boolean,
-      default: false,
+      default: false
     },
     date: {
       type: Date,
-      required: [true, 'Created date is required'],
+      required: [true, 'Created date is required']
     },
     updated: {
       type: Date,
-      default: Date.now(),
-    },
+      default: Date.now()
+    }
   },
-  { versionKey: '_v1' },
+  { versionKey: '_v1' }
 );
 
 const Note = mongoose.model('Note', NoteSchema);
